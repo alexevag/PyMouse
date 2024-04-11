1d3d503783cf7fe412f383392333c64b7c877b62
Alexandros Evangelou <alexandrose95@gmail.com>
2024-04-11 14:01:28 +0300

    update OpenField
    ger screen_width based the screen_size and use this to define the arena_size for the dlc
    
    move the wait for the dlc setup in the DLC __init __

--- Behaviors/OpenField.py
+++ Behaviors/OpenField.py
@@ -1,6 +1,6 @@
 import multiprocessing as mp
-import sys
 import time
+from typing import List
 
 import datajoint as dj
 import numpy as np
@@ -8,7 +8,7 @@ import numpy as np
 from core.Behavior import Behavior, behavior
 from Interfaces.camera import WebCam
 from Interfaces.dlc import DLC
-from utils.helper_functions import shared_memory_array
+from utils.helper_functions import get_display_width_height, shared_memory_array
 
 
 @behavior.schema
@@ -83,20 +83,29 @@ class OpenField(Behavior, dj.Manual):
         )
         self.shared_memory_shape = (1, 4)
 
-        # read screen parameters
-        # screen_params = self.logger.get(
-        #     table="SetupConfiguration.Screen",
-        #     key=f"setup_conf_idx={self.exp.params['setup_conf_idx']}",
-        #     as_dict=True,
-        # )[0]
-        # screen_height, screen_width = self.screen_dimensions(screen_params["size"])
-        # self.screen_size = int(screen_width * 10)  # mm
-        self.screen_size = 310
+        # get screen parameters
+        screen_params = self.logger.get(
+            table="SetupConfiguration.Screen",
+            key=f"setup_conf_idx={self.exp.params['setup_conf_idx']}",
+            as_dict=True,
+        )[0]
+        self.screen_width, _ = get_display_width_height(
+            screen_params["size"], screen_params["aspect"]
+        )
         self.screen_pos = np.array(
-            [[self.screen_size, 0], [self.screen_size, self.screen_size]]
+            [[self.screen_width, 0], [self.screen_width, self.screen_width]]
         )
 
+        self.response_locs = []
+        self.rew_locs = []
+        self.init_loc = []
+        self.position_tmst = 0
+        self.resp_position_idx = -1
+        self.affine_matrix = []
+        self.corners = []
+
     def setup(self, exp):
+        """setup is running one time at each session"""
         super(OpenField, self).setup(exp)
 
         # start camera recording process
@@ -113,15 +122,9 @@ class OpenField(Behavior, dj.Manual):
             model_path=self.exp.params["model_path"],
             shared_memory_shape=self.shared_memory_shape,
             logger=self.logger,
+            arena_size=self.screen_width,
         )
 
-        # wait until the dlc setup hass finished initialization before start the experiment
-        while not self.dlc.setup_ready.is_set():
-            sys.stdout.write(
-                "\rWaiting for the initialization of dlc" + "." * (int(time.time()) % 4)
-            )
-            sys.stdout.flush()
-            time.sleep(0.1)
         # save the corners/position
         self.affine_matrix, self.corners = self.dlc_queue.get()
         self.logger.put(
@@ -135,6 +138,7 @@ class OpenField(Behavior, dj.Manual):
         )
 
     def prepare(self, condition):
+        """prepare runs one time at the start of each a trial"""
         self.logged_pos = False
         self.in_location = False
         self._resp_loc = None
@@ -144,28 +148,28 @@ class OpenField(Behavior, dj.Manual):
 
         # find real position of the objects
         self.response_locs = self.screen_pos_to_real_pos(
-            self.curr_cond["response_loc_x"], const_dim=self.screen_size
+            self.curr_cond["response_loc_x"], const_dim=self.screen_width
         )
         self.rew_locs = self.screen_pos_to_real_pos(
-            self.curr_cond["reward_loc_x"], const_dim=self.screen_size
+            self.curr_cond["reward_loc_x"], const_dim=self.screen_width
         )
+        self.init_loc = [self.curr_cond["init_loc_x"], self.curr_cond["init_loc_y"]]
 
         self.position_tmst = 0
         self.resp_position_idx = -1
 
     def is_ready_start(self):
-        tmst, x, y, angle = self.pose[0]
-        r_x, r_y = self.curr_cond["init_loc_x"], self.curr_cond["init_loc_y"]
-        return (
-            np.sum((np.array([r_x, r_y]) - [x, y]) ** 2) ** 0.5
-            < self.curr_cond["init_radius"]
+        _, x_cur, y_cur, _ = self.pose[0]
+        return self.position_in_radius(
+            [x_cur, y_cur], self.init_loc, self.curr_cond["init_radius"]
         )
 
     def in_response_loc(self):
         """check if the animal position has been in a specific location"""
         self.tmst_cur, self.x_cur, self.y_cur, self.angle_cur = self.pose[0]
         # print("self.tmst_cur ", self.tmst_cur)
-        # check if pos in response location
+        # check if animal pos is in any response location
+
         self.resp_position_idx = self.position_in_radius(
             [self.x_cur, self.y_cur], self.response_locs, self.curr_cond["radius"]
         )
@@ -173,41 +177,41 @@ class OpenField(Behavior, dj.Manual):
             self._resp_loc = self.response_locs[self.resp_position_idx]
             self.in_location = True
             if not self.logged_pos:
-                act = {
-                    "animal_loc_x": self.x_cur,
-                    "animal_loc_y": self.y_cur,
-                    "time": self.tmst_cur,
-                    "in_pos": 1,
-                    "resp_loc_x": self._resp_loc[0],
-                    "resp_loc_y": self._resp_loc[1],
-                }
-                # print("act :", act)
-                key = {**self.logger.trial_key, **act}
-                self.logger.log("Activity", key, schema="behavior", priority=10)
-                self.logger.log("Activity.Touch", key, schema="behavior")
                 self.logged_pos = True
+                self.log_loc_activity(1)
 
         if self.in_location is True and self.resp_position_idx == -1:
             self.logged_pos = False
             self.in_location = False
-            act = {
-                "animal_loc_x": self.x_cur,
-                "animal_loc_y": self.y_cur,
-                "time": self.tmst_cur,
-                "in_pos": 0,
-                "resp_loc_x": self._resp_loc[0],
-                "resp_loc_y": self._resp_loc[1],
-            }
-            # print("act :", act)
-            key = {**self.logger.trial_key, **act}
-            self.logger.log("Activity", key, schema="behavior", priority=10)
-            self.logger.log("Activity.Touch", key, schema="behavior")
+            self.log_loc_activity(0)
 
         return self.resp_position_idx != -1
 
-    def position_in_radius(self, target_position, positions_list, radius):
+    def log_loc_activity(self, in_pos: int) -> None:
+        """Log activity with the given in_pos value.
+
+        Args:
+            in_pos (int): Flag indicating whether the animal is in a
+            specific position (1) or not (0).
+
+        Returns:
+            None
+        """
+        act = {
+            "animal_loc_x": self.x_cur,
+            "animal_loc_y": self.y_cur,
+            "time": self.tmst_cur,
+            "in_pos": in_pos,
+            "resp_loc_x": self._resp_loc[0],
+            "resp_loc_y": self._resp_loc[1],
+        }
+        key = {**self.logger.trial_key, **act}
+        self.logger.log("Activity", key, schema="behavior", priority=10)
+        self.logger.log("Activity.Touch", key, schema="behavior")
+
+    def position_in_radius(self, target_position, positions: List, radius):
         target_position = np.array(target_position)
-        positions_array = np.array(positions_list)
+        positions_array = np.array(positions)
 
         distances = np.linalg.norm(positions_array - target_position, axis=1)
         indices_within_radius = np.where(distances <= radius)[0]
@@ -219,6 +223,10 @@ class OpenField(Behavior, dj.Manual):
             return -1  # Return -1 if no position is within the radius
 
     def in_loc(self, is_reward):
+        # if "loc_type" in self.curr_cond and self.curr_cond["loc_type"] == "follow":
+        #     self.response_locs = self.obj_Pos(obj_id="all")
+        #     self.rew_loc = self.obj_Pos(obj_id=reward_obj)
+
         if self.in_response_loc():
             # if position_tmst==0 it it the first tmst in position set position_tmst to current time
             if self.position_tmst == 0:
@@ -228,13 +236,26 @@ class OpenField(Behavior, dj.Manual):
             # if is_reward=True check if resp_loc in reward locations
             # else if is_reward=False check if it belong to a response loc that is not reward
             if (resp_loc in self.rew_locs) == is_reward:
-                # check if the response ready time has passed
                 return True
         elif self.position_tmst != 0:
             # reset position_tmst=0 since is not in any response loc
             self.position_tmst = 0
 
         return False
+
+    # def update_loc(self):
+    #     """update the response/reward positions"""
+    #     if self.response.type == "object":
+    #         # find real position of the objects
+    #         self.response_locs = self.screen_pos_to_real_pos(
+    #             self.get_resp_obj_pos(self.resp_objs), self.screen_width
+    #         )
+    #         self.rew_locs = self.screen_pos_to_real_pos(
+    #             self.get_resp_obj_pos(self.reward_objs), self.screen_width
+    #         )
+
+    # def get_obj_pos(self, objs):
+    #     return [obj.get_x_Pos for obj in objs]
 
     def is_ready(self, ready_time):
         # if response_ready<=0 means there is no need to wait in reponse loc
@@ -268,8 +289,7 @@ class OpenField(Behavior, dj.Manual):
         Returns:
             bool: True if rewarded, False otherwise
         """
-
-        # # check that the last licked port ia also a reward port
+s
         licked_port = self.is_licking(since=tmst, reward=True)
         if licked_port:
             self.interface.give_liquid(licked_port)
@@ -285,8 +305,8 @@ class OpenField(Behavior, dj.Manual):
         # TODO: use response loc in the history
         self.update_history(self.response.port, punish=True)
 
-    def screen_pos_to_real_pos(self, pos, const_dim=215):
-        "Convert obj_pos in frame coordinates"
+    def screen_pos_to_real_pos(self, pos, const_dim):
+        "Convert obj_pos in real coordinates"
         screen_pos = self.screen_pos
         # in a square positions of the screen only one dimension is change
         diff_screen_pos = screen_pos[0, :] - screen_pos[1, :]
